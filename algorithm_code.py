9686b1e2b68fd7d85ee55f821ba9d7c4ade97b4d
John Williams <johnWilliams@MacBook-Pro-4.local>
2017-03-30 13:47:53 -0400

    parser function

--- algorithm_code.py
+++ algorithm_code.py
@@ -7,7 +7,7 @@ import random
 print("Welcome to the HAJ 1R Algorithm")
 print("Please enter the name of of an arff file that is present in the folder that you are running this program in:")
 
-file_name = input("File Name: ")
+file_name = "data.arff"
 print ("The File that you requested is " + file_name)
 
 
@@ -22,34 +22,30 @@ while checkexists == False:
     print (
         "Sorry, but it seems like your file does not exist in this folder. Please double check your spelling and try again.")
     print('Hint: Don\'t forget to put the document type extension.')
-    file_name = input("File Name: ")
+    file_name = "data.arff"
     print ("The File that you requested is " + file_name)
     checkexists = os.path.isfile(file_name)
     print("Does the file exits" + str(checkexists))  # can remove this later
 
 list_of_instances = []
-attributes = []  # this will need to be added to their programs
+attributes = []  # TODO: this will need to be added to their programs
 datavarcheck = "@data"
 datasection = False
 file = open(file_name, "r")
-# whole_file = file.read()
-# if '@data' in whole_file:
-#	print ("Checking if @data is in the whole file")
 for data in file:
 
-    print(data)
-    print ("this is before the if @data statement")
+    print "data: ", data
     if '@data' in data:
         datasection = True
         print(
             "did you get here")  # this if statment is not picking it up @data, it think it has something to do with reading lines
-    if data != None and datasection is True:
+    if data is not None and datasection is True:
         instance = data.split(",")
         # print("Your data is: " + data )
         # print (instance)
         list_of_instances.append(instance)
         # this appends to the greater list of instances
-        print("this is the list of instances")
-        print(list_of_instances)
     if datasection is False:
         attributes.append(data)  # this will need to be added to their programs
+print("this is the list of instances")
+print(list_of_instances[1][0])
---
+++ instanceBreaker.py
@@ -0,0 +1,6 @@
+# created by John Williams
+
+def spliter(instanceList):
+    for i in range(0, len(instanceList)):
+        for j in range(0, len(instanceList[i])): # double check this parameter
+            
