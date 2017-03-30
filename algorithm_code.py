8116105f2e4aa0ec92c6a9e5d8f9ed20f5ade2e2
hws025 <hilary.shea@girlswhocode.com>
2017-03-30 13:22:01 -0400

    Add files via upload

--- algorithm_code.py
+++ algorithm_code.py
@@ -5,61 +5,57 @@ import time
 import re
 import random
 
-print("Welcome to the HAJ 1R Algorithm")
+
+print("Welcome to the HAJ 1R Algorithm") 
 print("Please enter the name of of an arff file that is present in the folder that you are running this program in:")
 
-file_name = "data.arff"
+file_name = input("File Name: ")
 print ("The File that you requested is " + file_name)
 
-
-def split_instances(data):
-    print("This is you calling split instances")
+def split_instances(data): 
+	print("This is you calling split instances") 
+	
 
 
 checkexists = os.path.isfile(file_name)
 print("Does the file exits" + str(checkexists))
 
-while checkexists == False:
-    print (
-    "Sorry, but it seems like your file does not exist in this folder. Please double check your spelling and try again.")
-    print("Hint: Don't foget to put the document type extension.")
-    file_name = input("File Name: ")
-    print ("The File that you requested is " + file_name)
-    checkexists = os.path.isfile(file_name)
-    print("Does the file exits" + str(checkexists))
+while checkexists == False: 
+	print ("Sorry, but it seems like your file does not exist in this folder. Please double check your spelling and try again.")
+	print("Hint: Don't foget to put the document type extension.")
+	file_name = input("File Name: ")
+	print ("The File that you requested is " + file_name)
+	checkexists = os.path.isfile(file_name)
+	print("Does the file exits" + str(checkexists)) #can remove this later
+	
+	
+list_of_instances= []
+attributes = [] #this will need to be added to their programs
+datavarcheck = "@data"
+datasection = False
+file = open(file_name, "r") 
+#whole_file = file.read()
+#if '@data' in whole_file:
+#	print ("Checking if @data is in the whole file")
+for data in file:  
+	
+	print(data)
+	print ("this is before the if @data statement")
+	if '@data' in data:
+		datasection = True
+		print("did you get here") #this if statment is not picking it up @data, it think it has something to do with reading lines
+	if data	!= None and datasection is True: 
+		instance = data.split(",")
+		#print("Your data is: " + data )
+		#print (instance)
+		list_of_instances.append(instance) 
+		#this appends to the greater list of instances 
+		print("this is the list of instances")
+		print(list_of_instances)
+	if datasection is False: 
+		attributes.append(data)#this will need to be added to their programs
+		
+		
+		
 
-list_of_instances = []
-# datasection = False
-file = open(file_name, "r")
-whole_file = file.read()
-if '@data' in whole_file:
-    print ("Checking if @data is in the whole file")
-for data in file:
-    print "data: ", data
-    if "@data" in data:
-        # datasection = True
-        print(
-        "did you get here")  # this if statment is not picking it up @data, it think it has something to do with reading lines
-    if data != None:  # and datasection is True:
-        instance = data.split(",")
-        # print("Your data is: " + data )
-        # print (instance)
-        list_of_instances.append(instance)  # this appends to the greater list of instances
-
-    # print (str(list_of_instances[1]))
-
-
-    # file_split = file_contents.split("@data", 1)
-    # data = file_split[1]
-    # print(data)
-    # split_instances(data)
-    # file = open(file_name,"r")
-    # atdata= #this is a variable to test if the @data is in the data yet
-
-
-    #	for aline in file.readlines():
-    #		values = aline.split()
-    #		print 'QB ', values[0], values[1], 'had a rating of ', values[10]
-    #		if
-
-    # qbfile.close()
+		
