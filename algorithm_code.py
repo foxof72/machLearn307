14a888382a43a6ba31df0c4de720bdfed9bba71d
hws025 <hilary.shea@girlswhocode.com>
2017-04-03 22:10:37 -0400

    Add files via upload

--- algorithm_code.py
+++ algorithm_code.py
@@ -38,7 +38,9 @@ file = open(file_name, "r")
 # nominalAttributes = []
 # numericAttributes = []
 attribute_type_list = []
+namesOfNominalClasses = [] #I made this a string 
 # fullAttributeList = []  # TODO: add this in or no?
+all_attribute_names = []
 
 
 while True:
@@ -70,20 +72,23 @@ while True:
 
     # ToDo: have the user select what class they want to classify on
     # ToDo: here is where you should have a question and answer setting for what attributes it wants to classify an instance on
-
-for i in range(len(
-        attributes) - 1):  # we can put this into a function but this creates a list of if the attributes are numeric and nominal
-    if i > 1:
-        parse_attribute = attributes[i]
-        attribute_parts = parse_attribute.split(" ", 2)
-        attribute_name = attribute_parts[1]
-        attribute_type = attribute_parts[2]
-        if "numeric" in attribute_type:
-            attribute_type = "numeric"
-            attribute_type_list.append(attribute_type)
-        else:
-            attribute_type = "nominal"
-            attribute_type_list.append(attribute_type)
+def createListOfAttributes(attributes): 
+	for i in range(len(attributes) - 1):  # we can put this into a function but this creates a list of if the attributes are numeric and nominal
+		if i > 1:
+			parse_attribute = attributes[i]
+			attribute_parts = parse_attribute.split(" ", 2)
+			attribute_name = attribute_parts[1]
+			attribute_type = attribute_parts[2]
+			if "numeric" in attribute_type:
+				attribute_type = "numeric"
+				attribute_type_list.append(attribute_type)
+			
+			else:
+				attribute_type = "nominal"
+				attribute_type_list.append(attribute_type)
+				namesOfNominalClasses.append(attribute_name)
+			all_attribute_names.append(attribute_name)
+		
 
 
 # print("this is the list of the types and that attributes")
@@ -113,34 +118,31 @@ def getYN(listOfInstances):
     return yesCounter, noCounter
 
 
-def sortNominalAndNumeric(attribute_type_list,
-                          listOfInstances):  # the attribute_type_list[[attributename, attribute_type
+def sortNominalAndNumeric(attribute_type_list,listOfInstances):  # the attribute_type_list[[attributename, attribute_type
     # this function takes the sorting above and separates the values into numeric and nominal temp lists before pushing the whole instance into the greater list
     numAttr = len(listOfInstances[1])
     nominalInstanceList = []
     numericInstanceList = []
+	
     for i in range(len(listOfInstances)):  # this is the instances
         temp_numeric = []  # these are created so that the intances are still separated for our yes/no classifying function
         temp_nominal = []
+		
         for j in range(numAttr):  # this is the attributes
             if attribute_type_list[j] == "numeric":  # this is to create the numerical list
-                temp_numeric.append(float(listOfInstances[i][
-                                        j]))  # this is taking the instance value and the attribute and appending it to the temp list
+                temp_numeric.append(float(listOfInstances[i][j]))  # this is taking the instance value and the attribute and appending it to the temp list
             if attribute_type_list[j] == "nominal":  # this is to create the nominal list
-                temp_nominal.append(listOfInstances[i][
-                                        j])  # this is taking the instance value and the attribute and appending it to the list
+                temp_nominal.append(listOfInstances[i][j])  # this is taking the instance value and the attribute and appending it to the list
                 if (listOfInstances[i][j] == "yes\n" or listOfInstances[i][j] == "no\n"):
-                    temp_numeric.append(listOfInstances[i][
-                                            j])  # this is to provide for the case that yes/ no is nominal but should still be appended to the numeric
-        numericInstanceList.append(
-            temp_numeric)  # this is taking the instance value and the attribute and appending it to the list
-        nominalInstanceList.append(
-            temp_nominal)  # this is taking the instance value and the attribute and appending it to the list
+                    temp_numeric.append(listOfInstances[i][j])  # this is to provide for the case that yes/ no is nominal but should still be appended to the numeric
+        numericInstanceList.append(temp_numeric)  # this is taking the instance value and the attribute and appending it to the list
+        nominalInstanceList.append(temp_nominal)  # this is taking the instance value and the attribute and appending it to the list
     # print("this is the numeric instance list")
 
     # print(numericInstanceList)
     # print("this is a nominal instance list")
     # print(nominalInstanceList)
+	
     return numericInstanceList, nominalInstanceList
 
 
@@ -188,39 +190,29 @@ def classifer(listOfInstances):
         listOfAttributes.append(attributeValues)
         listOfYes.append(attributeValuesYes)
         listOfNo.append(attributeValuesNo)
-    # print("this is the list of yes")
-    # print (listOfYes)
-    # print("this is the list of no")
-    # print (listOfNo)
-    # print("this is the list of all the totals")
-    # print (listOfAttributes)
+    
     return listOfYes, listOfNo, listOfAttributes  # added two return values; yesCounter noCounter
 
 # John wrote this function to generate the fractions used in the final mathematical equation.  It should be in a loop.
 
 
-def fractionGenerator(attribute, randomVariableX, yesList, noList, yesTotal, noTotal):
-    print "start generation: "
-    print "yesTotal: " + str(yesTotal)
-    print "noTotal: " + str(noTotal)
-    try:  # checks if a value is numeric
-        float(attribute[1])
-        yesPDF, noPDF = numeric_value(yesList, noList, randomVariableX)
-        return yesPDF, noPDF
-    except ValueError as e:
-        yesDic = yesList[attribute[0]]
-        noDic = noList[attribute[0]]
-        print yesDic
-        print noDic
-        numYes = float(yesDic[attribute[1]])
-        print "numYes: " + str(numYes)
-        numNo = float(noDic[attribute[1]])
-        print "numNo: " + str(numNo)
-        yesFraction = float(numYes/yesTotal)
-        noFraction = float(numNo/noTotal)
-        print "yesFraction: " + str(yesFraction)
-        print "noFraction: " + str(noFraction)
-        return yesFraction, noFraction
+def fractionGenerator(attribute, yesList, noList, yesTotal, noTotal):
+    print ("start generation: ")
+    print ("yesTotal: " + str(yesTotal))
+    print ("noTotal: " + str(noTotal))
+    yesDic = yesList[attribute[0]]
+    noDic = noList[attribute[0]]
+    print (yesDic)
+    print (noDic)
+    numYes = float(yesDic[attribute[1]])
+    print ("numYes: " + str(numYes))
+    numNo = float(noDic[attribute[1]])
+    print ("numNo: " + str(numNo))
+    yesFraction = float(numYes/yesTotal)
+    noFraction = float(numNo/noTotal)
+    print ("yesFraction: " + str(yesFraction))
+    print ("noFraction: " + str(noFraction))
+    return (yesFraction, noFraction)
 # End of John's new function
 
 # John wrote this function to find what the odds are of each class.  It should be in a loop.
@@ -229,46 +221,46 @@ def fractionGenerator(attribute, randomVariableX, yesList, noList, yesTotal, noT
 def findStats(listOfKeys, yesList, noList, yesTotal, noTotal):
     noFrac = []  # all the fractions to be multiple together for no
     yesFrac =[]  # all the fractions to be multiple for yes
-    randomX = 0  # dummy for testing
-    print "\n\n"
-    print "find stats yes total: " + str(yesTotal)
-    print "find stats no total: " + str(noTotal)
+    print ("\n\n")
+    print ("find stats yes total: " + str(yesTotal))
+    print ("find stats no total: " + str(noTotal))
     yesTotal = float(yesTotal)
     noTotal = float(noTotal)
     yes = float(yesTotal/(yesTotal+noTotal))
     no = float(noTotal/(yesTotal+noTotal))
-    print "total yes odds: " + str(yes)
-    print "total no odds: " + str(no)
-    print "\n\n"
+    print ("total yes odds: " + str(yes))
+    print ("total no odds: " + str(no))
+    print ("\n\n")
     for i in range(0, len(listOfKeys)):  # this for loop loads the lists with fractions for calculations
-        yesOdd, noOdd = fractionGenerator(listOfKeys[i], randomX, yesList, noList, yesTotal, noTotal)
-        print "\n\n"
+        yesOdd, noOdd = fractionGenerator(listOfKeys[i], yesList, noList, yesTotal, noTotal)
+        print ("\n\n")
         yesFrac.append(yesOdd)
         noFrac.append(noOdd)
     yesFrac.append(yes)
     noFrac.append(no)
-    # float(None)
     yesChance = yesFrac[0]
-    print "yesChance: " + str(yesChance)
+    print ("yesChance: " + str(yesChance))
     noChance = noFrac[0]
-    print "noChance: " + str(noChance)
+    print ("noChance: " + str(noChance))
     for j in range(1, len(yesFrac)):  # does yes calculation
         yesChance = float(yesChance * yesFrac[j])
     for x in range(1, len(noFrac)):  # does no calculation
         noChance = float(noChance * noFrac[x])
     # now normalize
-    print "noChance later: " + str(noChance)
-    print "yesChance later: " + str(yesChance)
+    print ("noChance later: " + str(noChance))
+    print ("yesChance later: " + str(yesChance))
     yesNormal = yesChance/(yesChance + noChance) * 100
     noNormal = noChance/(yesChance + noChance) * 100
     return yesNormal, noNormal
 # end of John's calculation function
-
+createListOfAttributes(attributes)
 numericInstanceList, nominalInstanceList = sortNominalAndNumeric(attribute_type_list, list_of_instances)
 # this code is for testing
 numListYes, numListNo, numTotals = classifer(
     numericInstanceList)  # this is not working currently because yes\n and no\n are not in it
 nomListYes, nomListNo, nomTotals = classifer(nominalInstanceList)
+
+
 
 # print list_of_instances
 outYes, outNo = getYN(list_of_instances)
@@ -278,52 +270,74 @@ outYes, outNo = getYN(list_of_instances)
 listYes, listNo, listTotals = classifer(list_of_instances)
 testList = [[0, 'GP'], [1, 'F'], [19, 'yes']]
 yes, no = findStats(testList, listYes, listNo, outYes, outNo)
-print "\n\n "
-print "yes final: " + str(yes) + "%"
-print "no final: " + str(no) + "%"
+print ("\n\n ")
+print ("yes final: " + str(yes) + "%")
+print ("no final: " + str(no) + "%")
 # print("This is the num list yes")
 # print (numListYes)
 
+
+
+
 # this is where you start editing
-def userFacing(allAttributeList, AllListTotals):
-    print("What attribute would you like to classify on?")
-    print("Select on your keyboard what attribute you would like to classify on: ")
-    for i in range(len(allAttributeList)):
-        print(str(i) + ": " + allAttributeList[i])
-    keyboardInput = input()
-    if int(keyboardInput) < len(allAttributeList) and int(keyboardInput) > 0:
-        classifyBy = allAttributeList[int(keyboardInput)]
-    else:
-        keyboardInput = input("Sorry, that was out of range")
+def userFacing(allAttributeList, AllListTotals, namesOfNominalClasses):
+	instanceToBeClassified = []
+	classNumber = ""
+	print()
+	print("What attribute would you like to classify on?")
+	print("Select on your keyboard what attribute you would like to classify on: ")
+	for i in range(len(namesOfNominalClasses)):
+		name = str(namesOfNominalClasses[i])
+		#if name in namesOfNominalClasses:
+		print(str(i) + ": " + str(namesOfNominalClasses[i]))
+	keyboardInput = input()
+	while(int(keyboardInput) > len(namesOfNominalClasses) or int(keyboardInput) < 0):
+		keyboardInput = input("Sorry, that was out of range. Please insert another number:")
+	classifyBy = namesOfNominalClasses[int(keyboardInput)] #this is where you get what you classify by 
+	print("You are classifying on " + classifyBy + ".")
+	for i in range(len(allAttributeList)):
+		if classifyBy == str(allAttributeList[i]):
+			classNumber = i 
+			break
+			
 
-    print("You are classifying on " + classifyBy + ".")
-
-    print("Sweet! Now let's create an instance to classify:")
-    for j in range(len(allAttributeList)):
-        attrChoices = str(AllListTotals[j])
-        tempChoices = attrChoices.split("'")
+	print("Sweet! Now let's create an instance to classify:")
+	for j in range(len(allAttributeList)):
+		attrChoices = str(AllListTotals[j])
+		tempChoices = attrChoices.split("'")
         # print(tempChoices)
-        choices = []
-        for i in range(len(tempChoices)):
-            if ((i % 2) != 0):
-                choices.append(tempChoices[i])
+		choices = []
+		for i in range(len(tempChoices)):
+			if ((i % 2) != 0):
+				choices.append(tempChoices[i])
                 # print(tempChoices[i])
-        print("Please select what you would like to classify on for this attribute:")
-        for i in range(len(choices)):
-            print(str(i) + ": " + choices[i])
+		print("Please select what you would like to classify on for " + allAttributeList[j] + ":")
+		for i in range(len(choices)):
+			print(str(i) + ": " + choices[i])
 
-        choice = input()
-        if int(choice) > len(choices) or int(choice) < 0:
-            print("We are sorry, but that was out of range.")
-            for i in range(len(choices)):
-                print(str(i) + ": " + choices[i])
-            choice = input()
-        print(choice)
+		choice = input()
+		
+		while(int(keyboardInput) > len(namesOfNominalClasses) or int(keyboardInput) < 0):
+			choice = input("Sorry, that option is out of range:")
+			
+		#if int(choice) > len(choices) or int(choice) < 0:
+		#	print("We are sorry, but that was out of range.")
+		#	for i in range(len(choices)):
+		#		print(str(i) + ": " + choices[i])
+		#	choice = input()
+		#	while(
+		choice = int(choice)
+		temp_attribute = [choice, choices[choice]]
+		instanceToBeClassified.append(temp_attribute)
+		
+	return instanceToBeClassified
 
 # print output
 # print output
 # end testing
 
+
+#Anthony Green's changes
 def avg(attr):
     summation = 0
     for vals in attr:
@@ -332,24 +346,27 @@ def avg(attr):
 
 def sigma(attr, mean):
     variance = sum([pow(val - mean, 2) for val in attr]) / float(len(attr) - 1)
-    return math.sqrt(variance)
+    return math.sprt(variance)
 
 def pdf(x, mean, stdev):
     exponent = math.exp(-(math.pow(x - mean, 2) / (2 * math.pow(stdev, 2))))
     return (1 / (math.sqrt(2 * math.pi) * stdev)) * exponent
 
 def numeric_value(list_of_numerics, input_x):
-    probs = []
-    index = 0
-    for attr in list_of_numerics:
-        print str(attr)
+    probs = {}
+    for index, attr in list_of_numerics.iteritems():
+        print (str(index))#test
+        print (str(attr))#test
         probs[index] = 1
-        mean = float(avg(attr))
-        stdev = float(sigma(attr, mean))
-        print mean
-        print stdev
-        x = input_x[index]
-        probs[index] = float(pdf(x, mean, stdev))
-        print probs[index]
-        index += 1
+        for i in range(len(list_of_numerics)):
+            mean = avg(attr)
+            stdev = sigma(attr, mean)
+            print (mean) #test
+            print (stdev)#test
+            x = input_x[i]
+            probs[index] *= pdf(x, mean, stdev)
     return probs
+	
+
+instanceForClassification= userFacing(all_attribute_names, listTotals, namesOfNominalClasses)
+print(instanceForClassification)
